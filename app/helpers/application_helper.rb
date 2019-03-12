749976619572803862ccfb5ae912b54ccbf971ae
takuma_bishan.abewadana <takuma_bishan.abewadana@supership.jp>
2019-03-12 13:44:30 +0900

    [Add]#5 Add export csv function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def google_url(title)
     "https://www.google.com/search?q=" + title.gsub(/\s/, "+")
   end
+
+  def export_csv(path)
+    link_to("Export CSV", "#{path}.csv", class: "btn btn-outline-success")
+  end
 end
