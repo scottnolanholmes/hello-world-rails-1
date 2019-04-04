f1809ecd17e792005e57f45b71dfea3bcf5ee85f
belion freee <34331230+belion-freee@users.noreply.github.com>
2019-04-04 20:43:26 +0900

    removed extra method

--- spec/mock_helper.rb
+++ spec/mock_helper.rb
@@ -15,14 +15,5 @@ module MockHelper
         headers: { "Content-Type" => "application/json" }
       )
     end
-
-    def encrypt_token(data)
-      iv = SecureRandom.hex(8)
-      cipher = OpenSSL::Cipher.new("aes-256-cbc")
-      cipher.encrypt
-      cipher.key = ENV["AES_KEY"]
-      cipher.iv  = iv
-      [Base64.strict_encode64(iv), Base64.strict_encode64(cipher.update(data) + cipher.final), "200"].join(",")
-    end
   end
 end
